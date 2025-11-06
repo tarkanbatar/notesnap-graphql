d64dd71729fb66da4c608a4c12a1ff4b9003411a
Tarkan Batar <tarkanbatar4@gmail.com>
2025-11-06 22:35:10 +0300

    password hashing added

--- database/models/User.js
+++ database/models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const mongoose = require('../database');
 const { Schema, model } = mongoose;
 
@@ -5,6 +6,13 @@ const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
 });
 
 const User = model('User', userSchema);
